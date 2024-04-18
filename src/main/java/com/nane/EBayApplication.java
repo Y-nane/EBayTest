9c0a16e5b43bc74c9eba905cc7674253287a0f52
fanyuan <499112607@qq.com>
2024-04-18 23:40:50 +0800

    init

---
+++ src/main/java/com/nane/EBayApplication.java
@@ -0,0 +1,11 @@
+package com.nane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EBayApplication.class,args);
+    }
+}
